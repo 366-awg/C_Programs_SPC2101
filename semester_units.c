1ec9ee1999d217b72095b2654f198683578e47ff
366-awg <annwangare36@gmail.com>
2025-09-19 16:15:15 +0300

    Add program to display all semester units

---
+++ semester_units.c
@@ -0,0 +1,20 @@
+/*Name: Ann Wangari Githinji
+Reg No: CT101/G/26510/25
+Date: 19/09/2025
+Description: A program to display all the units for this semester*/
+#include <stdio.h>
+
+int main() {
+    //Displaying semester units
+    printf("Semester Units:\n");
+    printf("Introduction to Computer Architecture\n");
+    printf("Physics\n");
+    printf("Introduction to Programing and Problem Solving\n");
+    printf("Discrete Mathematics\n");
+    printf("Mathematics for Science\n");
+    printf("Fundamentals of ICT\n");
+    printf("Analogue Electronics\n");
+    printf("Communication Skills\n");
+
+    return 0;
+}
